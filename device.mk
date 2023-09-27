12b2391d2430c939deb2d2a3f581f2ea4c5d3a81
Subhashis2007 <baradsubhashis@gmail.com>
2023-09-27 05:33:36 +0530

    Add GrapheneOS camera

--- device.mk
+++ device.mk
@@ -35,6 +35,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	MtkInCallService
 
+PRODUCT_PACKAGES += \
+        GrapheneCamera
+
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
 	$(DEVICE_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
