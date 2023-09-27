f8d5b67863efe3b582d78a9bdb0d6ede51298419
Subhashis2007 <baradsubhashis@gmail.com>
2023-09-27 06:06:39 +0530

    PixelOS Bringup

--- aosp_RMX1821.mk
+++ aosp_RMX1821.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from RMX1821 device makefile
 $(call inherit-product, device/realme/RMX1821/device.mk)
 
-# LineageOS Stuff.
+# AOSP Stuff.
 TARGET_EXCLUDES_AUDIOFX := true
 
 # Boot Animation
@@ -23,7 +23,7 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX1821
+PRODUCT_NAME := aosp_RMX1821
 PRODUCT_DEVICE := RMX1821
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := RMX1821
