4c2fdcceb2693bbe4175e2ce79dbc3e09f645eed
Iverz <iverzasterix@gmail.com>
2023-11-29 16:38:32 +0000

    RMX1821: AfterLifePrkjt BringUp!

--- afterlife_RMX1821.mk
+++ afterlife_RMX1821.mk
@@ -8,13 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AfterLife stuff.
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Inherit from RMX1821 device makefile
 $(call inherit-product, device/realme/RMX1821/device.mk)
 
-# LineageOS Stuff.
+# AfterLife flags
+AFTERLIFE_MAINTAINER := IverzAsterix
+
+# Offline Charging
+USE_PIXEL_CHARGING := true
+
+# disable/enable blur support, default is false
+TARGET_SUPPORTS_BLUR := true
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# AfterLife Stuff.
 TARGET_EXCLUDES_AUDIOFX := true
 
 # Boot Animation
@@ -23,7 +35,7 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX1821
+PRODUCT_NAME := afterlife_RMX1821
 PRODUCT_DEVICE := RMX1821
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := RMX1821
